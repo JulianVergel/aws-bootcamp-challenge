ed8c3ed6766ae205461f6b16ed9c8d63f4c15d34
JulianVergel <julian.vergel@pragma.com.co>
2025-08-01 16:18:40 -0500

    Primer commit

---
+++ monolith-api/src/test/java/com/bootcamp_aws/monolith_api/MonolithApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp_aws.monolith_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
