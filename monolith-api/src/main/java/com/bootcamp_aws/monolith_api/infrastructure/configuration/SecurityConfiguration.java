083abea62368a5fb13a9e4e907c6e1dba9248c96
JulianVergel <julian.vergel@pragma.com.co>
2025-08-06 11:14:38 -0500

    HU03 terminada correctamente

---
+++ monolith-api/src/main/java/com/bootcamp_aws/monolith_api/infrastructure/configuration/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.bootcamp_aws.monolith_api.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
